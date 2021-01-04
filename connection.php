e33115164fadb0ebf8fa328d382b924f740b4df3
aabhiabhishek <61412556+aabhiabhishek@users.noreply.github.com>
2021-01-04 18:50:58 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,5 +2,5 @@
 
     error_reporting( E_ALL );
     ini_set( "display_errors", 1 );
-    $conn=mysqli_connect('database-1.cn2jsrntxupz.ap-south-1.rds.amazonaws.com','admin','Snowbear123#','db') or die ('Connection was not established');
+    $conn=mysqli_connect('database-1.cn2jsrntxupz.ap-south-1.rds.amazonaws.com','admin','Snowbear123#','mini') or die ('Connection was not established');
 ?>
